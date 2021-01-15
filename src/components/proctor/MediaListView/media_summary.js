f2eb1a5e31168fb240b42102e65531a4a198b7e3
charmi2701 <ckdm27@gmail.com>
2021-01-15 15:34:57 +0530

    Added verification buttons

--- src/components/proctor/MediaListView/media_summary.js
+++ src/components/proctor/MediaListView/media_summary.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-const MediaSummary = ({data}) => {
+const MediaSummary = (props) => {
+    const imageArray = []
+    Object.keys(props.data).forEach(image => {
+        imageArray.push(props.data[image])
+    })
+
+    const deleteTriggerRecord = () => {
+
+    }
+
+    const disqualifyStudent = () => {
+
+    }
+
+    //console.log(imageArray)
     return (
         <>
         <li className="media mx-5 my-3 border">
-            <Link to={'/imagedetail/' + data.key}>
-                <img class="mr-3" src={data.value.imageURL} style={{borderRadius:0, height:75, width:75 }} alt="triggered User examsecure"/>
+            <Link to={'/imagedetail/' + props.testnumber+ '/' + props.name}>
+                <img class="mr-3" src={imageArray[0].imageURL} style={{borderRadius:0, height:50, width:70 }} alt="triggered User examsecure"/>
             </Link>
                 <div class="media-body ">
-                    <h5 class="mt-0 mb-1" style={{fontWeight:'bold'}}>{data.key}</h5>
-                    {data.value.reason}
+                    <h5 class="mt-0 mb-1" style={{fontWeight:'bold'}}>{props.name}</h5>
+                    Reason
                 </div>
-            {/* <a href="#" className="btn btn-outline-success">Verify</a>
-            <a href="#" className="btn btn-outline-success disqualify">Disqualify</a> */}
+            <a href="#" className="btn btn-outline-success" onClick={deleteTriggerRecord}>Verify</a>
+            <a href="#" className="btn btn-outline-success disqualify" onClick={disqualifyStudent}>Disqualify</a>
         </li>
         </>
     )
